b017aab9c6d54abc07726a9e832f040fd41db560
Thomas Harvey <tom@alush.co.uk>
2018-02-21 16:32:41 -0600

    linting for whitespace

--- energy_py/tests/test_tf.py
+++ energy_py/tests/test_tf.py
@@ -12,20 +12,28 @@ epsilon = 1e-5
 
 config = {'input_shape': (2,),
           'layers': (4, 8),
-          'output_shape':(8,),
+          'output_shape': (8,),
           'learning_rate': 0.1}
 
 input_shape = config['input_shape']
 layers = config['layers']
 output_shape = config['output_shape']
 
-def weight(shape): return np.ones(shape)
-def bias(shape): return np.ones(shape)
-def relu(x): return x * (x > 0)
+
+def weight(shape):
+    return np.ones(shape)
+
+
+def bias(shape):
+    return np.ones(shape)
+
+
+def relu(x):
+    return x * (x > 0)
 
 observation = np.random.uniform(size=input_shape).reshape(1, -1)
 action = np.array([0, 1]).reshape(1, 2)
-target = np.ones((1,1))
+target = np.ones((1, 1))
 
 
 def test_train_op():
@@ -84,7 +92,7 @@ def test_network():
         q = Qfunc(config, scope='test')
         sess.run(tf.global_variables_initializer())
         feed_dict = {q.observation: observation.reshape(1, *input_shape),
-                     q.action: action} 
+                     q.action: action}
 
         q_vals = sess.run(q.q_values, feed_dict)
 
@@ -102,7 +110,7 @@ def test_network():
         feed_dict[q.target] = target
         tf_error = sess.run(q.error, feed_dict)
 
-        q_val = out[action[0][0]] #  TODO this is a bit hacky!
+        q_val = out[action[0][0]]  # TODO this is a bit hacky!
         error = target - q_val
         diff = tf_error - error
         assert np.less(diff, epsilon).all()
