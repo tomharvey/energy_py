a8a7a38ee908c1d567cbf9a2f47a22a59d321686
Thomas Harvey <tom@alush.co.uk>
2018-02-21 16:32:16 -0600

    no need for variable assignment to _

--- energy_py/tests/test_processors.py
+++ energy_py/tests/test_processors.py
@@ -5,19 +5,20 @@ from energy_py import Normalizer, Standardizer
 #  make a batch with one dimensional samples
 batch = np.random.rand(10, 6)
 
+
 def test_normalizer():
     norm = Normalizer()
-    
-    _ = norm.transform(batch[1:5])
-    _ = norm.transform(batch[5:])
+    norm.transform(batch[1:5])
+    norm.transform(batch[5:])
 
     assert norm.maxs.all() == np.max(batch, axis=0).all()
+
 
 def test_standardizer():
     std = Standardizer()
 
-    _ = std.transform(batch[1:5])
-    _ = std.transform(batch[5:])
+    std.transform(batch[1:5])
+    std.transform(batch[5:])
 
     assert std.means.all() == np.mean(batch, axis=0).all()
     assert std.stds.all() == np.std(batch, axis=0).all()
